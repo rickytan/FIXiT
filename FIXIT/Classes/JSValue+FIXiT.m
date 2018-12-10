a5b6a20db48f9ff6ec0125e20b534b73d4259d58
rickytan <ricky.tan.xin@gmail.com>
2018-12-10 21:05:19 +0800

    feat: 避免干扰其他 JSContext

--- FIXIT/Classes/JSValue+FIXiT.m
+++ FIXIT/Classes/JSValue+FIXiT.m
@@ -16,24 +16,22 @@
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-//        method_exchangeImplementations(class_getClassMethod(self, @selector(valueWithObject:inContext:)), class_getClassMethod(self, @selector(__fixit_valueWithObject:inContext:)));
         method_exchangeImplementations(class_getInstanceMethod(self, @selector(toObject)), class_getInstanceMethod(self, @selector(__fixit_toObject)));
         method_exchangeImplementations(class_getInstanceMethod(self, @selector(toObjectOfClass:)), class_getInstanceMethod(self, @selector(__fixit_toObjectOfClass:)));
     });
 }
 
-//+ (JSValue *)__fixit_valueWithObject:(id)value inContext:(JSContext *)context
-//{
-//    if (context != [FIXIT context]) {
-//        return [self __fixit_valueWithObject:value inContext:context];
-//    }
-//    return [context.globalObject[@"makeProxiedObject"] callWithArguments:@[value ?: [NSNull null]]];
-//}
-
 - (id)__fixit_toObject
 {
+    if (self.context != [FIXiT context]) {
+        return [self __fixit_toObject];
+    }
+
     if (self.isUndefined) {
         return nil;
+    }
+    if (self.isNull) {
+        return [NSNull null];
     }
 
     JSValue *target = self[@"__target__"];
@@ -45,8 +43,16 @@
 
 - (id)__fixit_toObjectOfClass:(Class)expectedClass
 {
+    if (self.context != [FIXiT context]) {
+        return [self __fixit_toObjectOfClass:expectedClass];
+    }
+
     if (self.isUndefined) {
         return nil;
+    }
+
+    if (self.isNull) {
+        return [NSNull null];
     }
 
     JSValue *target = self[@"__target__"];
