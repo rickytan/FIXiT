952ea4345301a5f3533f61da183ce37d41900b47
rickytan <ricky.tan.xin@gmail.com>
2018-12-10 11:42:13 +0800

    feat: update demo

--- Example/FIXIT/RTViewController.m
+++ Example/FIXIT/RTViewController.m
@@ -25,23 +25,23 @@
     self.title = @"Text VC";
 
     // Do any additional setup after loading the view, typically from a nib.
-    //    [[FIXIT fix] executeScript:@"var fix = new FIXIT('RTViewController');\nvar origin = fix.fixInstanceMethod('_crash:', function(self) {\n    console.log(self);\n    console.log(arguments);\n    return \"success\";\n});\n\nconsole.log(origin);"];
     [[FIXIT fix] executeScript:
      JSString
      (\n
       require('UIColor');\n
       var fix = Fixit.fix('RTViewController');\n
       var origin = fix.instanceMethod('locationOf:atIndex:defaultValue:', function(locations, index, point) {\n
-         console.log(arguments);\n
-         this.view.backgroundColor = UIColor.redColor;\n
-         this.button['setTitle:forState:']('test_title', 0);\n
-         this.button.frame = CGRectMake(100, 200, 120, 80);\n
+         var vc = this.navigationController.presentingViewController.title;\n
+         console.log(vc, this.title);
+         this.button().frame = CGRectMake(100, 200, 120, 80);\n
          if (index > locations.length - 1) {\n
+             this.button['setTitle:forState:']('out of bounds', 0);\n
+             this.view.backgroundColor = UIColor.redColor;\n
              return point;\n
          }\n
-         var val = locations[index].CGPointValue;\n
-         console.log(val, val.__target__);\n
-         return val;\n
+         this.button['setTitle:forState:']('in bounds', 0);\n
+         this.view.backgroundColor = UIColor.yellowColor;\n
+         return locations[index];\n
      });\n
       )];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
@@ -49,10 +49,10 @@
             CGPoint p = [self locationOf:@[[NSValue valueWithCGPoint:CGPointMake(-0.33, 1.28)]] atIndex:2 defaultValue:CGPointMake(0.5, 1.5)];
             NSLog(@"%@", NSStringFromCGPoint(p));
         }
-        {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             CGPoint p = [self locationOf:@[[NSValue valueWithCGPoint:CGPointMake(-0.33, 1.28)]] atIndex:0 defaultValue:CGPointMake(0.5, 1.5)];
             NSLog(@"%@", NSStringFromCGPoint(p));
-        }
+        });
     });
 }
 
