91e18a5dc3251ff5f5cf5dae61aa764c9d0c3686
rickytan <ricky.tan.xin@gmail.com>
2018-12-15 13:31:25 +0800

    feat: check nil

--- Example/FIXIT/patch.js
+++ Example/FIXIT/patch.js
@@ -2,8 +2,22 @@
   require('UIColor, UIAlertView, UIApplication, UINavigationController, UIViewController, RTCustomModel');
   var fix = Fixit.fix('RTViewController');
   fix.instanceMethod('locationOf:atIndex:defaultValue:', function (locations, index, point) {
-    var vc = this.navigationController.presentingViewController.title;
-    console.log(vc, this.title);
+    var vc = this;
+    if (isNil(vc)) {
+      console.log(7);
+    }
+    vc = vc.navigationController;
+    if (isNil(vc)) {
+      console.log(11);
+    }
+    vc = vc.presentingViewController;
+    if (isNil(vc)) {
+      console.log(15);
+    }
+    vc = vc.title;
+    if (isNil(vc)) {
+      console.log(19);
+    }
     this.button.frame = CGRectMake(100, 200, 120, 80);
     if (index > locations.length - 1) {
       this.button['setTitle:forState:']('out of bounds', 0);
@@ -14,10 +28,11 @@
     this.view.backgroundColor = UIColor.yellowColor;
     return locations[index];
   });
+
   var viewDidLoad = fix.instanceMethod('viewDidLoad', function () {
     viewDidLoad.apply(this, arguments);
     var that = this;
-    dispatch_after(2, function() {
+    dispatch_after(2, function () {
       that.button.removeFromSuperview;
       that.button = nil;
       RTCustomModel.new;
