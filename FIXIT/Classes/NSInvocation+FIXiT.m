72bcc927ff3ad37d091dda79cf91825b857e7580
rickytan <ricky.tan.xin@gmail.com>
2019-09-26 13:13:08 +0800

    fix: typo

--- FIXIT/Classes/NSInvocation+FIXiT.m
+++ FIXIT/Classes/NSInvocation+FIXiT.m
@@ -96,7 +96,7 @@
     const char *argType = [self.methodSignature getArgumentTypeAtIndex:index];
     // Skip const type qualifier.
     if (argType[0] == _C_CONST) argType++;
-#define READ_ANS_SET(type, method) do { type val = [obj method]; [self setArgument:&val atIndex:(NSInteger)index]; } while (0)
+#define READ_AND_SET(type, method) do { type val = [obj method]; [self setArgument:&val atIndex:(NSInteger)index]; } while (0)
     if (strcmp(argType, @encode(id)) == 0 || strcmp(argType, @encode(Class)) == 0) {
         __autoreleasing id returnObj = obj;
         [self setArgument:&returnObj atIndex:(NSInteger)index];
@@ -108,35 +108,35 @@
         [self setArgument:&theClass atIndex:(NSInteger)index];
         // Using this list will box the number with the appropriate constructor, instead of the generic NSValue.
     } else if (strcmp(argType, @encode(char)) == 0) {
-        READ_ANS_SET(char, charValue);
+        READ_AND_SET(char, charValue);
     } else if (strcmp(argType, @encode(int)) == 0) {
-        READ_ANS_SET(int, intValue);
+        READ_AND_SET(int, intValue);
     } else if (strcmp(argType, @encode(short)) == 0) {
-        READ_ANS_SET(short, shortValue);
+        READ_AND_SET(short, shortValue);
     } else if (strcmp(argType, @encode(long)) == 0) {
-        READ_ANS_SET(long, longValue);
+        READ_AND_SET(long, longValue);
     } else if (strcmp(argType, @encode(long long)) == 0) {
-        READ_ANS_SET(long long, longLongValue);
+        READ_AND_SET(long long, longLongValue);
     } else if (strcmp(argType, @encode(unsigned char)) == 0) {
-        READ_ANS_SET(unsigned char, unsignedCharValue);
+        READ_AND_SET(unsigned char, unsignedCharValue);
     } else if (strcmp(argType, @encode(unsigned int)) == 0) {
-        READ_ANS_SET(unsigned int, unsignedIntValue);
+        READ_AND_SET(unsigned int, unsignedIntValue);
     } else if (strcmp(argType, @encode(unsigned short)) == 0) {
-        READ_ANS_SET(unsigned short, unsignedShortValue);
+        READ_AND_SET(unsigned short, unsignedShortValue);
     } else if (strcmp(argType, @encode(unsigned long)) == 0) {
-        READ_ANS_SET(unsigned long, unsignedLongValue);
+        READ_AND_SET(unsigned long, unsignedLongValue);
     } else if (strcmp(argType, @encode(unsigned long long)) == 0) {
-        READ_ANS_SET(unsigned long long, unsignedLongLongValue);
+        READ_AND_SET(unsigned long long, unsignedLongLongValue);
     } else if (strcmp(argType, @encode(float)) == 0) {
-        READ_ANS_SET(float, floatValue);
+        READ_AND_SET(float, floatValue);
     } else if (strcmp(argType, @encode(double)) == 0) {
-        READ_ANS_SET(double, doubleValue);
+        READ_AND_SET(double, doubleValue);
     } else if (strcmp(argType, @encode(BOOL)) == 0) {
-        READ_ANS_SET(BOOL, boolValue);
+        READ_AND_SET(BOOL, boolValue);
     } else if (strcmp(argType, @encode(bool)) == 0) {
-        READ_ANS_SET(BOOL, boolValue);
+        READ_AND_SET(BOOL, boolValue);
     } else if (strcmp(argType, @encode(char *)) == 0) {
-        READ_ANS_SET(const char *, pointerValue);
+        READ_AND_SET(const char *, pointerValue);
     } else if (strcmp(argType, @encode(void (^)(void))) == 0) {
         __unsafe_unretained id block = obj;
         [self setArgument:&block atIndex:(NSInteger)index];
@@ -153,7 +153,7 @@
         }
         [self setArgument:valueBytes atIndex:(NSInteger)index];
     }
-#undef READ_ANS_SET
+#undef READ_AND_SET
 }
 
 - (NSArray *)fixit_arguments {
@@ -263,7 +263,7 @@
     const char *argType = self.methodSignature.methodReturnType;
     // Skip const type qualifier.
     if (argType[0] == _C_CONST) argType++;
-#define READ_ANS_SET(type, method) do { type val = [value method]; [self setReturnValue:&val]; } while (0)
+#define READ_AND_SET(type, method) do { type val = [value method]; [self setReturnValue:&val]; } while (0)
     if (strcmp(argType, @encode(id)) == 0 || strcmp(argType, @encode(Class)) == 0) {
         __autoreleasing id returnObj = value.toObject;
         [self setReturnValue:&returnObj];
@@ -275,33 +275,33 @@
         [self setReturnValue:&theClass];
         // Using this list will box the number with the appropriate constructor, instead of the generic NSValue.
     } else if (strcmp(argType, @encode(char)) == 0) {
-        READ_ANS_SET(char, toInt32);
+        READ_AND_SET(char, toInt32);
     } else if (strcmp(argType, @encode(int)) == 0) {
-        READ_ANS_SET(int, toInt32);
+        READ_AND_SET(int, toInt32);
     } else if (strcmp(argType, @encode(short)) == 0) {
-        READ_ANS_SET(short, toInt32);
+        READ_AND_SET(short, toInt32);
     } else if (strcmp(argType, @encode(long)) == 0) {
-        READ_ANS_SET(long, toInt32);
+        READ_AND_SET(long, toInt32);
     } else if (strcmp(argType, @encode(long long)) == 0) {
-        READ_ANS_SET(long long, toDouble);
+        READ_AND_SET(long long, toDouble);
     } else if (strcmp(argType, @encode(unsigned char)) == 0) {
-        READ_ANS_SET(unsigned char, toUInt32);
+        READ_AND_SET(unsigned char, toUInt32);
     } else if (strcmp(argType, @encode(unsigned int)) == 0) {
-        READ_ANS_SET(unsigned int, toUInt32);
+        READ_AND_SET(unsigned int, toUInt32);
     } else if (strcmp(argType, @encode(unsigned short)) == 0) {
-        READ_ANS_SET(unsigned short, toUInt32);
+        READ_AND_SET(unsigned short, toUInt32);
     } else if (strcmp(argType, @encode(unsigned long)) == 0) {
-        READ_ANS_SET(unsigned long, toUInt32);
+        READ_AND_SET(unsigned long, toUInt32);
     } else if (strcmp(argType, @encode(unsigned long long)) == 0) {
-        READ_ANS_SET(unsigned long long, toDouble);
+        READ_AND_SET(unsigned long long, toDouble);
     } else if (strcmp(argType, @encode(float)) == 0) {
-        READ_ANS_SET(float, toDouble);
+        READ_AND_SET(float, toDouble);
     } else if (strcmp(argType, @encode(double)) == 0) {
-        READ_ANS_SET(double, toDouble);
+        READ_AND_SET(double, toDouble);
     } else if (strcmp(argType, @encode(BOOL)) == 0) {
-        READ_ANS_SET(BOOL, toBool);
+        READ_AND_SET(BOOL, toBool);
     } else if (strcmp(argType, @encode(bool)) == 0) {
-        READ_ANS_SET(BOOL, toBool);
+        READ_AND_SET(BOOL, toBool);
     } else if (strcmp(argType, @encode(char *)) == 0) {
         const char * str = [[value toObject] pointerValue];
         [self setReturnValue:&str];
@@ -321,7 +321,7 @@
         }
         [self setReturnValue:&valueBytes];
     }
-#undef READ_ANS_SET
+#undef READ_AND_SET
 }
 
 @end
